39ee7002487f7b6d0edbefc7af4a1a1b0ecbe928
SamiaMALKI <malki.samia@hotmail.fr>
2015-11-23 21:17:56 +0100

    cc

--- samiaMALKI/connect.php
+++ samiaMALKI/connect.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "thyp_cartogame";
+$dbname = "thyp_cartogamme";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
