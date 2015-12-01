06a43ce0e79ed8846d55681435455ed1d1955fd8
abdel1314 <bouloujour.abderrahmane@gmail.com>
2015-12-01 10:06:36 +0100

    new version

--- ettanass/carte/php/connect.php
+++ ettanass/carte/php/connect.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "thyp_cartogame";
+$dbname = "aaa";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
