6e4b3db82c0b4595846c6219af42cfe552c7de02
xiaohe <xiaohe.cao@gmail.com>
2015-12-01 11:38:17 +0100

    map

--- visite/php/d.php
+++ visite/php/d.php
@@ -2,7 +2,7 @@
 include_once 'connect.php';
 
 switch ($_GET["table"]) {
-	case "score":
+	case "scores":
 		deleteScore($_GET);
 		break;
 	case "personne":
@@ -10,7 +10,16 @@ switch ($_GET["table"]) {
 		break;
 	case "document":
 		deleteDocument($_GET);
-		break;		
+		break;	
+	case "personnesw2ui":
+		deletePersonnesForW2UIGrid();
+		break;
+	case "documentsw2ui":
+		deleteDocumentsForW2UIGrid();
+		break;
+	case "scoresw2ui":
+		deleteScoresForW2UIGrid();
+		break;
 	default:
 		;
 	break;
@@ -18,44 +27,151 @@ switch ($_GET["table"]) {
 
 function deleteScore($data){
 	global $conn;
+	$count = $data["count"];
+	$sql = "DELETE FROM scores where  id_scores ='".$data["id_0"]."'";
 	
-	// sql to delete a record
-	$sql = "DELETE FROM scores WHERE id_scores=".$data["id"]." ";
+	if($count > 1){
+		
+		for($i = 1 ; $i < $count ; $i++){
+		    $sql .=" OR id_scores ='".$data["id_$i"]."'";
+		}
+	}
 
+	//echo $sql;
 	if ($conn->query($sql) === TRUE) {
-	    echo "Score deleted successfully";
+	    echo "score deleted successfully";
 	} else {
-	    echo "Error deleting record: " . $conn->error;
+	    echo "Error: " . $sql . "<br>" . $conn->error;
 	}	
 }
 
 function deletePersonne($data){
 	global $conn;
 	
-	// sql to delete a record
-	$sql = "DELETE FROM personnes WHERE id_perso=".$data["id"]." ";
-
+	$sql = "DELETE FROM personnes where ( id_perso =".$data["id_perso"].")";
+	//echo $sql;
 	if ($conn->query($sql) === TRUE) {
-	    echo "Personne deleted successfully";
+	    echo "personne deleted successfully";
 	} else {
-	    echo "Error deleting record: " . $conn->error;
+	    echo "Error: " . $sql . "<br>" . $conn->error;
 	}	
 }
 
 function deleteDocument($data){
 	global $conn;
 	
-	// sql to delete a record
-	$sql = "DELETE FROM documents WHERE id_doc=".$data["id"]." ";
-
+	$sql = "DELETE FROM documents where ( id_doc =".$data["id_doc"].")";
+	//echo $sql;
 	if ($conn->query($sql) === TRUE) {
-	    echo "Document deleted successfully";
+	    echo "document deleted successfully";
 	} else {
-	    echo "Error deleting record: " . $conn->error;
+	    echo "Error: " . $sql . "<br>" . $conn->error;
 	}	
 }
 
+// W2ui get functions
+function deletePersonnesForW2UIGrid(){
+	global $conn;
+	$list= array();
+	
+	$sql = "DELETE FROM personnes where ";
+	$action = (isset($_POST["cmd"])) ? $_POST["cmd"] : null;
+	
+	if($action!=null && $action ="delete-records")
+	{
+		$id_scores = $_POST["selected"];
+		$sql .= " id_perso = $id_scores[0]";
+		
+		if(count($id_scores) > 1)
+		{
+			for($i = 1 ; $i < count($id_scores) ; $i++)
+			{
+				$sql .=" OR id_perso = $id_scores[$i] ";
+			}
+		}
+		
+		if ($conn->query($sql) === TRUE) 
+		{	
+			$list["status"] = "success";
+			echo json_encode($list);
+		} 
+		else 
+		{
+			$list["status"] = "error";
+			$list["message"] = "aucun element n'est supprimé";
+			echo json_encode($list);
+		}
+	}
+}
 
 
+function deleteDocumentsForW2UIGrid(){
+	global $conn;
+	$list= array();
+	
+	$sql = "DELETE FROM documents where ";
+	$action = (isset($_POST["cmd"])) ? $_POST["cmd"] : null;
+	
+	if($action!=null && $action ="delete-records")
+	{
+		$id_scores = $_POST["selected"];
+		$sql .= " id_doc = $id_scores[0]";
+		
+		if(count($id_scores) > 1)
+		{
+			for($i = 1 ; $i < count($id_scores) ; $i++)
+			{
+				$sql .=" OR id_doc = $id_scores[$i] ";
+			}
+		}
+		
+		if ($conn->query($sql) === TRUE) 
+		{	
+			$list["status"] = "success";
+			echo json_encode($list);
+		} 
+		else 
+		{
+			$list["status"] = "error";
+			$list["message"] = "aucun element n'est supprimé";
+			echo json_encode($list);
+		}
+	}
+}
+			
+			
+function deleteScoresForW2UIGrid(){
+	global $conn;
+	$list= array();
+	
+	$sql = "DELETE FROM scores where ";
+	$action = (isset($_POST["cmd"])) ? $_POST["cmd"] : null;
+	
+	if($action!=null && $action ="delete-records")
+	{
+		$id_scores = $_POST["selected"];
+		$sql .= " id_scores = $id_scores[0]";
+		
+		if(count($id_scores) > 1)
+		{
+			for($i = 1 ; $i < count($id_scores) ; $i++)
+			{
+				$sql .=" OR id_scores = $id_scores[$i] ";
+			}
+		}
+		
+		if ($conn->query($sql) === TRUE) 
+		{	
+			$list["status"] = "success";
+			echo json_encode($list);
+		} 
+		else 
+		{
+			$list["status"] = "error";
+			$list["message"] = "aucun element n'est supprimé";
+			echo json_encode($list);
+		}
+	}
+}
 
 $conn->close();
---
+++ visite/php/u.php
@@ -0,0 +1,77 @@
+<?php
+include_once 'connect.php';
+
+switch ($_GET["table"] || $_POST["table"]) {
+	case "score":
+		updateScore($_GET);
+		break;
+	case "personne":
+		updatePersonne($_GET);
+		break;
+	case "document":
+		updateDocument($_GET);
+		break;	
+	case "personnesw2ui":
+		updatePersonnesForW2UIGrid();
+		break;
+	default:
+		;
+	break;
+}
+
+function updateScore($data){
+	global $conn;
+	$sql = "update scores set distance=".$data["distance"].", maj = NOW() where id_scores =".$data["id_score"];
+
+	//echo $sql;
+	if ($conn->query($sql) === TRUE) {
+	    echo "score updated successfully";
+	} else {
+	    echo "Error: " . $sql . "<br>" . $conn->error;
+	}	
+}
+
+function updatePersonne($data){
+	global $conn;
+	
+	$sql = "update personnes set nom= ".$data["nom"]." where id_perso=".$data["id_perso"];
+	//echo $sql;
+	if ($conn->query($sql) === TRUE) {
+	    echo "personne updated successfully";
+	} else {
+	    echo "Error: " . $sql . "<br>" . $conn->error;
+	}	
+}
+
+function updateDocument($data){
+	global $conn;
+	// should format latlng in js
+	$sql = "update documents set nom= ".$data["nom"]." latlng=".$data["latlng"]." url=".$data["url"]." where  id_doc =".$data["id_doc"];
+	//echo $sql;
+	if ($conn->query($sql) === TRUE) {
+	    echo "document updated successfully";
+	} else {
+	    echo "Error: " . $sql . "<br>" . $conn->error;
+	}	
+}
+
+function updatePersonnesForW2UIGrid()
+{
+	global $conn;
+	$list= array();
+	
+	$changes = $_POST["changed"];
+	
+	foreach($changes as $row)
+	{
+		$sql = " update personnes set nom = '".$row["text"]."' where id_perso = ".$row["recid"];
+		
+		$conn->query($sql);
+	}
+	
+	$list["status"] = "success";
+	echo json_encode($list);
+	
+}
+
+$conn->close();
