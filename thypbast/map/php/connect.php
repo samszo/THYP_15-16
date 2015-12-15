0ef29fffbe6632e4f75124cdcc11523997f60bc5
thypbast <zaky.zeroual@gmail.com>
2015-12-15 19:27:52 +0100

    .

--- thypbast/map/php/connect.php
+++ thypbast/map/php/connect.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "thypbast";
+$dbname = "thyp_cartogame";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
