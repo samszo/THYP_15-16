91cb5747132f1a5dccb0ce99cd0bac00e681f620
mkrayem <krayem.mabrouka@gmail.com>
2015-12-16 11:21:27 +0100

    examen

--- mkrayem/krayemMabrouka/r.php
+++ mkrayem/krayemMabrouka/r.php
@@ -1,42 +1,43 @@
-<?php 
+
+<?php
 include_once 'connect.php';
-
-
-		readpersonnes();
-	
-		readcolis();
-function readpersonnes(){
-	global $conn;
-	
+switch ($_GET["table"]) {
+	case "personnes":
+		selectpersonne($_GET);
+		break;
+	case "colis":
+		selectcolis($_GET);
+		break;	
+	default:
+		;
+	break;
+}
+function selectpersonne($data){
 	$sql = "SELECT * FROM personnes";
 	$result = $conn->query($sql);
+	//echo $sql;
 	if ($result->num_rows > 0) {
     // output data of each row
-		echo "<h1 style='text-align:center;'>Table Personnes</h1>";
-		echo "<table class='table table-hover'>
-				 <thead>
-      					<tr class='danger'>
-					        
-					        <th>Id personne</th>
-					        <th>Nom de la personne</th>
-							
-					       <th>Prenom de la personne</th>
-     					 </tr>
-   				 </thead><tbody> ";
+	
 	    while($row = $result->fetch_assoc()) {
-	        echo "<tr class='info'><td> " . $row["id_perso"]. "</td><td> " . $row["nom"]. "</td><td> " . $row["prenom"]. "</td></tr>";
+	        echo json_encode($row);
 	    }
-	     echo "</tbody>
-  </table>";
 	} else {
-    	echo "Table personne vide<br>";
-	}
+    	echo "0 results";
+	}	
 }
-function readcolis(){
+function selectcolis($data){
 	global $conn;
 	
-	$sql = "SELECT * FROM colis";
+	$sql = "SELECT * FROM colis where id_personne =".$data["id_personne"];
+	
 	$result = $conn->query($sql);
+	if ($result->num_rows > 0) {
+		while($row = $result->fetch_assoc()) {
+			echo json_encode($row);
+			
+		}
+	}
 }
-
+$conn->close();
 ?>
