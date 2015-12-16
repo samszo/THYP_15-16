15f39d070fda4566d1b78ef9d5568d74c8777845
Elalami90 <mouad.elalami90@gmail.com>
2015-12-16 08:56:08 +0000

    Exam

--- Elalami90/map/php/connect.php
+++ Elalami90/map/php/connect.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "elalami90";
+$dbname = "cartogame";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
