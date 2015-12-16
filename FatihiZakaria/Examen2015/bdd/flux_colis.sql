cea9fa652f25b4f4506e48bad7adcfae1a7f1348
FatihiZakaria <fatihi259@gmail.com>
2015-12-16 12:08:59 +0100

    commit

--- FatihiZakaria/Examen2015/bdd/flux_colis.sql
+++ FatihiZakaria/Examen2015/bdd/flux_colis.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Mer 16 Décembre 2015 à 10:08
+-- Généré le :  Mer 16 Décembre 2015 à 12:08
 -- Version du serveur :  5.6.17
 -- Version de PHP :  5.5.12
 
@@ -31,7 +31,16 @@ CREATE TABLE IF NOT EXISTS `colis` (
   `descreptif` varchar(255) NOT NULL,
   `idpersonne` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Contenu de la table `colis`
+--
+
+INSERT INTO `colis` (`id`, `descreptif`, `idpersonne`) VALUES
+(1, 'desc1', 1),
+(2, 'desc2', 2),
+(3, 'desc3', 3);
 
 -- --------------------------------------------------------
 
@@ -46,7 +55,16 @@ CREATE TABLE IF NOT EXISTS `personne` (
   `email` varchar(255) NOT NULL,
   `adresse` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Contenu de la table `personne`
+--
+
+INSERT INTO `personne` (`id`, `nom`, `prenom`, `email`, `adresse`) VALUES
+(1, 'Fatihi', 'zakaria', 'fatihi259@gmail.com', '22 rue de l''aigle,92250'),
+(2, 'Ouali', 'Oussama', 'oussama@gmail.com', '23 rue paris 75000 '),
+(3, 'nom1', 'prenom1', 'email@email.com', 'azert');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
