276db9a33420737399ca7b511895dd3a0861984c
FatihiZakaria <fatihi259@gmail.com>
2016-04-04 22:23:16 +0200

    Revert "majmap"
    This reverts commit f9cf502761fb34353ec08c943642886649e69dcc.

--- thypbast/map/mapgame/api map.php
+++ thypbast/map/mapgame/api map.php
@@ -26,7 +26,7 @@ include "mykey.php";
 	 <div id="img">
 	 	 Trouvez la ville d'origine de ce document: 
 <br><br>
-	 <img id="Img" width="250px" height="250px" />
+	 <img class = "imag" id="Img" src="images/google.jpg"  align="center" width=100px height=100px/>
 	 </div> <br>
 	 
 	<label id="compteur"></label><br>
@@ -34,10 +34,9 @@ include "mykey.php";
 	 </div>
 	 
 	 <div id="map"></div>
-     <div id="score"></div>
+
     <script type="text/javascript">
 	
-
 	function JoueurExiste() {
 		  var name;
           name = document.getElementById("nom").value;
@@ -59,32 +58,22 @@ include "mykey.php";
 			var point;
 			var compteur=5;
 			var distance=0;
-			var place;
-			var places;
-			var lieu=0;
-			var listPerso;
-			
+
 			function initMap() {
-			  	
-			  
 			  var haightAshbury = {lat: 37.775091, lng: -122.418922};
 			  document.getElementById("compteur").innerHTML="il vous reste "+ compteur + "chances";
 			  document.getElementById("dis").innerHTML="la distance entre votre choix et l'emplacement est de "+distance+ " km";
-			  
 			  map = new google.maps.Map(document.getElementById('map'), {
 				zoom: 3,
 				center: haightAshbury,
 				mapTypeId: google.maps.MapTypeId.TERRAIN
 			  });
-			  
 				point = new google.maps.LatLng({lat: 39.002673,lng:-122.596810});
-				
-			  
+			  // This event listener will call addMarker() when the map is clicked.
 			  map.addListener('click', function(event) {
 				  deleteMarkers();
-				  addMarker(event.latLng);
+				addMarker(event.latLng);
 			  });
-			  
 				var triangleCoords = [
 				{lat: 39.002673, lng: -122.596810},
 				{lat: 37.427623, lng: -123.168233},
@@ -112,7 +101,7 @@ include "mykey.php";
 				  }
 				});
 			  });
-			  reaDocument();
+			  
 			}
 
 			// Adds a marker to the map and push to the array.
@@ -125,17 +114,13 @@ include "mykey.php";
 			  markers.push(marker);
 			  distance = parseInt( google.maps.geometry.spherical.computeDistanceBetween( point, markers[markers.length-1].getPosition())/1000);
 			  compteur--;
-			
 			  if (distance < 200){
 				alert ("VOUS AVEZ GAGNÉ");
 				compteur=5;
-				reaDocument();
-				
 			  }
 			  if (compteur==0){
 				alert("VOUS AVEZ PERDU! RECOMMENCEZ");
 				compteur =5;
-				reaDocument();
 			  
 			  }
 			document.getElementById("compteur").innerHTML="il vous reste "+ compteur + "chances";
@@ -173,52 +158,13 @@ include "mykey.php";
 							$.ajax({
 							  url: 'r.php',
 							  data: data,
-							  success: function(html) {
-							  listPerso = JSON.parse(html);
-
-							  }
 							});
 						  });
 						  
 						}
 
-			function reaDocument(){
-			  data = {"table":"document"};
-			  $.ajax({url:'r.php',
-				  data: data ,
-				  success: function (html){
-					  places = JSON.parse(html);
-					  changeDoc();
-					  }
-				  });
 
-				 }
-		
-		    function changeDoc(){
 
-				place = places[lieu];
-				var x = +place["lat"];
-				var y = +place["lng"];
-				 
-				//$("#place").html(place["nom"]); 
-				point = new google.maps.LatLng(x,y);
-				
-				document.getElementById('Img').src = place.url;
-				if (lieu < (places.length -1)) 
-				{
-					lieu++;
-				}
-				else{
-					lieu=0;
-				}
-			 
-			}
-
-			function creaScore(){
-				data.table = "score";
-                $.get('c.php',
-                data);
-			}
     </script>
 	
     <script async defer
